d8987cda7a23b84a309725667c2f2b9124b9dc9c
khanhhuy123 <113548392+khanhhuy123@users.noreply.github.com>
2023-02-14 12:59:52 +0100

    Update ContactList.kt

--- ContactList.kt
+++ ContactList.kt
@@ -15,8 +15,28 @@ class ContactList{
 
     fun showContactList(){
         //Ska visas i alfabetisk ordning
+        println("Contact List:")
+        contactList.sortBy { it.firstName }
+
+        for((i) in contactList.withIndex()){
+            if(contactList[i].phoneNumber1 == "0"){
+                println(contactList[i].firstName + "\n" + contactList[i].lastName + "\n" + contactList[i].mailAddress)
+                println("------------------------------------------------------")
+            }
+            else if(contactList[i].phoneNumber2 == "0"){
+                println(contactList[i].firstName + "\n" + contactList[i].lastName + "\n" + contactList[i].phoneNumber1 +
+                        "\n" + contactList[i].mailAddress)
+                println("------------------------------------------------------")
+            }
+            else if(contactList[i].phoneNumber3 == "0"){
+                println(contactList[i].firstName + "\n" + contactList[i].lastName + "\n" + contactList[i].phoneNumber1 + "\n" +
+                        contactList[i].phoneNumber2 + "\n" + contactList[i].mailAddress)
+                println("------------------------------------------------------")
+            }
+            else{
+                println(contactList[i].firstName + "\n" + contactList[i].lastName + "\n" + contactList[i].phoneNumber1 + "\n" +
+                        contactList[i].phoneNumber2 + "\n" + contactList[i].phoneNumber3 + "\n" + contactList[i].mailAddress)
+                println("------------------------------------------------------")
+            }
     }
-
-
-
 }
