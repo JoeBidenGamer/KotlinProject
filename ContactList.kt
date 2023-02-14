d32b70994050fd7f6ff3e10e1944226b4d276f77
khanhhuy123 <113548392+khanhhuy123@users.noreply.github.com>
2023-02-14 12:22:47 +0100

    Add files via upload

---
+++ ContactList.kt
@@ -0,0 +1,22 @@
+class ContactList{
+    private val contactList = mutableListOf<Person>()
+
+    fun addContact(person: Person){
+        contactList.add(person)
+    }
+
+    fun removeContact(person: Person){
+
+    }
+
+    fun editContact(person:Person){
+
+    }
+
+    fun showContactList(){
+        //Ska visas i alfabetisk ordning
+    }
+
+
+
+}
