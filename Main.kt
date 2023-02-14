3f2cb4ba229ac876dda85bad837fa44fcd70049a
khanhhuy123 <113548392+khanhhuy123@users.noreply.github.com>
2023-02-14 12:27:47 +0100

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,6 +1,24 @@
 fun main(args: Array<String>) {
 
+    /*val person = Person("firstName2323", "lastName", "phone1", "phone2", "phone3", "Mail")
 
+    val contactList = mutableListOf<Person>()
+
+
+    contactList.add(person)
+
+    println("Person 1: ")
+    println(contactList[0].firstName)
+    println(contactList[0].lastName)
+    println(contactList[0].phoneNumber1)
+    println(contactList[0].phoneNumber2)
+    println(contactList[0].phoneNumber3)
+    println(contactList[0].mailAddress)
+    println("Person 2: ")
+    println(contactList[1].firstName)
+    println(contactList[1].lastName)
+    println(contactList[1].phoneNumber1)
+    println(contactList[1].mailAddress)*/
 
     println("Program arguments: ${args.joinToString()}")
 }
