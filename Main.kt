eec4e06940c83b6178cc117a70a5322d72c512d3
khanhhuy123 <113548392+khanhhuy123@users.noreply.github.com>
2023-02-14 13:24:34 +0100

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,17 +1,38 @@
 fun main(args: Array<String>) {
-
-    val person = Person("Eva", "lastName", "07312345678", "0", "0", "Mail@gmail.com")
-    val person2 = Person("Adam", "lastName22", "0723456784", "01273122", "0", "Mail2@gmail.com")
-    val person3 = Person("David", "lastName22", "0723456784", "01273122", "012321314", "Mail2@gmail.com")
+    var i: Int
+    var firstName: String
+    var lastName: String
+    var phoneNum1: String
+    var phoneNum2: String
+    var phoneNum3: String
+    var mail: String
+    
     val contactList = ContactList()
+    do{
+        println("1. Add contact")
+        println("2. Remove contact")
+        println("3. Edit contact")
+        println("4. Show contact list")
+        println("0. Exit")
+        i = Integer.valueOf(readln())
 
-    contactList.addContact(person)
-    contactList.addContact(person2)
-    contactList.addContact(person3)
-    contactList.showContactList()
-    contactList.removeContact(1)
-    println("Newlist: \n")
-    contactList.showContactList()
+        if(i == 1){
+            print("Person firstname: ")
+            firstName = readln()
+            print("Person lastname: ")
+            lastName = readln()
+            print("Person phone-number 1 (if there is no phone number input 0): ")
+            phoneNum1 = readln()
+            print("Person phone-number 2 (if there is no phone number input 0): ")
+            phoneNum2 = readln()
+            print("Person phone-number 3 (if there is no phone number input 0): ")
+            phoneNum3 = readln()
+            print("Person mail: ")
+            mail = readln()
+            contactList.addContact(Person(firstName, lastName, phoneNum1, phoneNum2, phoneNum3, mail))
+        }
+    }
+    while (i != 0)
 
     println("Program arguments: ${args.joinToString()}")
 }
