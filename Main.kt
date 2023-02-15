aa183dd929a6816e9c61fb7b8fc093bcc4822307
khanhhuy123 <113548392+khanhhuy123@users.noreply.github.com>
2023-02-15 09:58:31 +0100

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,13 +1,21 @@
 fun main(args: Array<String>) {
     var i: Int
+    var ID: Int
     var firstName: String
     var lastName: String
     var phoneNum1: String
     var phoneNum2: String
     var phoneNum3: String
     var mail: String
-    
+
+    //Objekt contactList skapas
     val contactList = ContactList()
+    //Lägger till 3 personer i kontakt lista
+    contactList.addContact(Person("David", "lastName" , "1234567", "0", "0", "David@gmail.com"))
+    contactList.addContact(Person("Adam", "lastName2", "9876543", "9374683", "0", "Adam@gmail.com"))
+    contactList.addContact(Person("Eva", "lastName3", "4676543", "235345", "3456456", "Eva@gmail.com"))
+
+    //Meny med val
     do{
         println("1. Add contact")
         println("2. Remove contact")
@@ -31,8 +39,24 @@ fun main(args: Array<String>) {
             mail = readln()
             contactList.addContact(Person(firstName, lastName, phoneNum1, phoneNum2, phoneNum3, mail))
         }
+        else if(i == 2){
+            print("Person ID: ")
+            ID = Integer.valueOf(readln())
+            contactList.removeContact(ID)
+        }
+        else if(i == 3){
+            print("Person ID: ")
+            ID = Integer.valueOf(readln())
+            contactList.editContact(ID)
+        }
+        else if(i == 4){
+            contactList.showContactList()
+        }
     }
     while (i != 0)
+
+
+
 
     println("Program arguments: ${args.joinToString()}")
 }
